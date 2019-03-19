ed949a3873ff53b5498102a89d2ee63b7073cbfb
Gabriel Trindade <gfktrin@yahoo.com.br>
2019-03-19 01:04:33 -0300

    HomeScreen cards

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import { Container, Button, Row, Col, Card } from 'react-materialize';
+import { Container, Button, Row, Col, Card, Icon } from 'react-materialize';
 import Hero from '../components/hero/hero';
+import './homeScreen.scss';
 
 const HomeScreen = () => (
   <main>
@@ -9,8 +10,23 @@ const HomeScreen = () => (
     <Container>
       <Row>
         <Col m={4} s={12}>
-          <Card className='blue-grey darken-1' textClassName='white-text' title='Card title'>
-          I am a very simple card.
+          <Card className='white darken-1' textClassName='black-text' title={<div class="card-title"><Icon medium className="more-black">attach_money</Icon><h4>Preço</h4></div>}>
+            Oferecemos cursos completos com preço acessível
+            <br />
+            <br />
+            <br />
+          </Card>
+        </Col>
+        <Col m={4} s={12}>
+          <Card className='white darken-1' textClassName='black-text' title={<div class="card-title"><Icon medium className="more-black">thumb_up_alt</Icon><h4>Qualidade</h4></div>}>
+            Aprenda com quem está no mercado, usando as tecnologias mais recentes para garantir o máximo de recursos
+            <br />
+            <br />
+          </Card>
+        </Col>
+        <Col m={4} s={12}>
+          <Card className='white darken-1' textClassName='black-text' title={<div class="card-title"><Icon medium className="more-black">timeline</Icon><h4>Crescimento</h4></div>}>
+            Nossos cursos abordam os assuntos com profundidade, de forma a garantir o máximo de aprendizado possível dos alunos
           </Card>
         </Col>
       </Row>
