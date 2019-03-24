f9c44e66dc7dd89bf383aab1114e16052bfa2c23
Gabriel Trindade <gfktrin@yahoo.com.br>
2019-03-24 03:42:43 -0300

    folder name fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Routes from './components/Routes/routes';
+import Routes from './components/routes/routes';
 
 class App extends Component {
   render() {
