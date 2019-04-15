9ecff461fbae005f84a56fbccea72ad2bcc5624a
Gabriel Trindade <gfktrin@yahoo.com.br>
2019-04-15 15:31:09 -0300

    Fix router problem on build

--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import HomeScreen from '../../screens/homeScreen/homeScreen';
 import AboutScreen from '../../screens/aboutScreen/aboutScreen';
 import ShopScreen from '../../screens/shopScreen/shopScreen';
 import LoginScreen from '../../screens/loginScreen/loginScreen';
 
 const Routes = () => (
-  <Switch>
+  <Router>
     <Route exact path='/' component={HomeScreen}/>
     <Route path='/empresa' component={AboutScreen}/>
     <Route path='/loja' component={ShopScreen}/>
     <Route path='/login' render={LoginScreen}/>
-  </Switch>
+  </Router>
 );
 
 export default Routes;
