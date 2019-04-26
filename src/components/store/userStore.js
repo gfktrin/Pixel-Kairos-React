f139a16a64658874845cee57507905324d708bc1
Gabriel Trindade <gfktrin@yahoo.com.br>
2019-04-26 00:25:41 -0300

    persist state with localstorage

--- src/components/store/userStore.js
+++ src/components/store/userStore.js
@@ -1,8 +1,28 @@
 import { createConnectedStore } from 'undux';
 
-export default createConnectedStore({
+const initialState = {
   id: undefined,
   name: undefined,
   courses: undefined,
   selectedCourse: undefined,
-});
+};
+
+function withLocalStorage(store) {
+  window.addEventListener('unload', () =>
+    window.localStorage.setItem('unduxState', JSON.stringify(store.getState()))
+  )
+  return store
+}
+
+function fromLocalStorage(initialState) {
+  let savedState = JSON.parse(window.localStorage.getItem('unduxState'))
+  if (savedState === null) {
+    return initialState
+  }
+  return {...initialState, ...savedState}
+}
+
+export default createConnectedStore(
+  fromLocalStorage(initialState),
+  withLocalStorage
+);
