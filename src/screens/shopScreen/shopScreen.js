09afef569056af22a9fc2cbf3e11474674f7ced0
Gabriel Trindade <gfktrin@yahoo.com.br>
2019-06-03 22:36:29 -0300

    fix typo

--- src/screens/shopScreen/shopScreen.js
+++ src/screens/shopScreen/shopScreen.js
@@ -23,7 +23,7 @@ const ShopScreen = () => (
       <Row>
         <Col m={4} s={12}>
           <CourseCard courseName='Curso de desenvolvimento de games'
-              courseDescription='Já sonhou em criar seu proprio jogo? Com esse curso, você vai aprender a criar jogos de diferentes tipos com várias ferramentas, de forma que qualquer um possa tirar seu projeto do papel.' />
+              courseDescription='Já sonhou em criar seu próprio jogo? Com esse curso, você vai aprender a criar jogos de diferentes tipos com várias ferramentas, de forma que qualquer um possa tirar seu projeto do papel.' />
         </Col>
       </Row>
     </Container>
